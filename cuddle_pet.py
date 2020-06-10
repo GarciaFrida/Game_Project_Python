da19e1289b356837ad32e6e09222239cdc1bfabb
Frida Garcia <garciafrida09@gmail.com>
2020-06-10 15:35:47 -0400

    more additions to cuddle_pet.py

--- cuddle_pet.py
+++ cuddle_pet.py
@@ -1,4 +1,16 @@
 from virtual_pet import Pet
 
 class CuddlyPet(Pet):
-        pass 
+    def cuddle(self, other_pet):
+        other_pet.get_love()
+
+
+
+daisy = CuddlyPet("Daisy", 50, 20, 20 ,1)
+bobby = Pet("bobby", 50, 10, 30, 10)
+
+print(bobby.happiness)
+
+daisy.cuddle(bobby)
+
+print(bobby.happiness)
