478d1afbad51d05fddb7db5e0100e47db43542d0
Frida Garcia <garciafrida09@gmail.com>
2020-06-10 13:56:38 -0400

    Class for Pets

---
+++ pet_class.py
@@ -0,0 +1,7 @@
+class Pet():
+    def __init__(self, name, fullness, happiness, loneliness):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = happiness
+        self.loneliness = loneliness
+
