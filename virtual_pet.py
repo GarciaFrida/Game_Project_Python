be136b99bd89f040c5c5b663bead5b6cde0a5693
Frida Garcia <garciafrida09@gmail.com>
2020-06-12 13:37:01 -0400

    Spelling issues and syntax issues resolved

--- virtual_pet.py
+++ virtual_pet.py
@@ -47,11 +47,12 @@ def main():
         choice = get_user_choice(main_menu)
         if choice == 1:
             pet_name = input("What would you like to name your pet? ")
-            print("Please choose they type of pet:")
+            print("Please choose the type of pet:")
+            type_choice = get_user_choice(adoption_menu)
             if type_choice == 1:
                 pets.append(Pet(pet_name))
             elif type_choice == 2:
-                pets_append(CuddlyPet(pet_name))
+                pets.append(CuddlyPet(pet_name))
             print("You now have %d pets" % len(pets))
         if choice == 2:
             for pet in pets:
